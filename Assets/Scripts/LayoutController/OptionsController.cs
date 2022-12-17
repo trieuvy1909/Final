02f23d775aeadd2b8871d0f2c0262e65fbcea70d
Lê Triệu Vỷ <95388691+trieuvy1909@users.noreply.github.com>
2022-12-18 02:35:47 +0700

    mucsic

--- Assets/Scripts/LayoutController/OptionsController.cs
+++ Assets/Scripts/LayoutController/OptionsController.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class OptionsController : MonoBehaviour
 {
     public void soundSettingON(){
-    
+        AudioSource[] audios = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource a in audios)
+        {
+            a.Play();
+        }
     }
     public void soundSettingOFF(){
-    
+        AudioSource[] audios = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource a in audios)
+        {
+            a.Pause();
+        }
     }
     public void ReturnMenu(){
         SceneManager.LoadScene("Menu");
