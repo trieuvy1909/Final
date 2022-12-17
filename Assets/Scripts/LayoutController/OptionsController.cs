a0789cdb971e18fd699f50d5f1367158ab9927a4
Lê Triệu Vỷ <95388691+trieuvy1909@users.noreply.github.com>
2022-12-18 03:04:33 +0700

    .

--- Assets/Scripts/LayoutController/OptionsController.cs
+++ Assets/Scripts/LayoutController/OptionsController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class OptionsController : MonoBehaviour
 {
+    public Button btnOn;
+    public Button btnOff;
     public void soundSettingON(){
         AudioSource[] audios = FindObjectsOfType<AudioSource>();
         foreach (AudioSource a in audios)
